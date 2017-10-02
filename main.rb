8aad2e7e1911809379a9396e711a5bc9a7755427
Henri Brinkhuis <henri@htbr.nl>
2017-10-02 23:05:54 +0200

    WIP reserve row before actual row

--- main.rb
+++ main.rb
@@ -7,23 +7,24 @@ require 'logger'
 
 host = 'https://www.pathe.nl'
 show_id = '2308082'
-ticket_start_path = "/tickets/start/#{show_id}"
-ticket_transaction_path = '/api/transactions/'
+seat_id = '766089'
+
+transaction_start_path = "/tickets/start/#{show_id}"
+transaction_api_path = '/api/transactions/'
 
 a = Mechanize.new do |agent|
   agent.user_agent_alias = 'Mac Safari'
   logger = Logger.new STDOUT
-  logger.level = Logger::INFO
+  logger.level = Logger::DEBUG
   agent.log = logger
-
 end
 
-ticket_list_page = a.post("#{host}#{ticket_start_path}")
+ticket_list_page = a.post("#{host}#{transaction_start_path}")
 
 seat_page_link = ticket_list_page.links.select { |z| z.to_s == 'Stap 2: Stoel kiezen' }[0]
 transaction_id = (%r{tickets\/([A-Z0-9]*)\/}.match seat_page_link.href)[1]
 
-ticket_put_uri = "#{host}#{ticket_transaction_path}#{transaction_id}"
+ticket_put_uri = "#{host}#{transaction_api_path}#{transaction_id}"
 ticket_put_response = a.put(ticket_put_uri,
                             { 'tickets': [{ 'type': '382738', 'number': '6' }] }.to_json,
                             'Referer' => "#{host}/tickets/#{transaction_id}",
@@ -33,11 +34,17 @@ ticket_put_response = a.put(ticket_put_uri,
 
 seat_page = seat_page_link.click
 
-free_seat_number = seat_page.parser.css("#seats li[class!='seat-sold']")
-                       .css("li[class!=' seat-handicapped']")
-                       .first[:id]
+seat_element = seat_page.parser.css("#seats li[id='#{seat_id}']")
 
-seat_put_uri = "#{host}/tickets/#{transaction_id}/seats/#{free_seat_number}"
+seats = seat_page.parser.css('#seats li')
+current_seat_row = (%r{top: (\d+)px}.match seat_element[0][:style])[1].to_i
+seat_rows = (seats.collect { |z| (%r{top: (\d+)px}.match z[:style])[1].to_i}).uniq.sort
+seat_previous_row = seat_rows[seat_rows.index(current_seat_row)-1]
+seat_next_row = seat_rows[seat_rows.index(current_seat_row)+1]
+
+front_seat = (seats.select { |z| z[:style].include? "top: #{seat_previous_row}" })[0][:id]
+
+seat_put_uri = "#{host}/tickets/#{transaction_id}/seats/#{front_seat}"
 
 seat_put_response = a.put(seat_put_uri, '')
-pp seat_put_response
+# pp seat_put_response
